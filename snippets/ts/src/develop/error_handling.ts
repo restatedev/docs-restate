615457188d6046e1a4bafe6f03679697333dabab
Giselle van Dongen <giselle@restate.dev>
2025-07-25 15:46:31 +0200

    Add WIP error handling page

--- snippets/ts/src/develop/error_handling.ts
+++ snippets/ts/src/develop/error_handling.ts
@@ -5,9 +5,33 @@ const service = restate.service({
   name: "ErrorHandling",
   handlers: {
     greet: async (ctx: restate.Context, name: string) => {
-      // <start_here>
+      // <start_terminal>
       throw new TerminalError("Something went wrong.", { errorCode: 500 });
-      // <end_here>
+      // <end_terminal>
     },
   },
 });
+
+// <start_as_terminal>
+class MyValidationError extends Error {}
+
+const greeter = restate.service({
+  name: "greeter",
+  handlers: {
+    greet: async (ctx: restate.Context, name: string) => {
+      if (name.length === 0) {
+        throw new MyValidationError("Length too short");
+      }
+      return `Hello ${name}`;
+    }
+  },
+  options: {
+    asTerminalError: (err) => {
+      if (err instanceof MyValidationError) {
+        // My validation error is terminal
+        return new restate.TerminalError(err.message, {errorCode: 400})
+      }
+    }
+  }
+});
+// <end_as_terminal>
