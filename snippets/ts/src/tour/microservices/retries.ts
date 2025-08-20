82ecf1dba42aa341c936dcdbf11519844d404c4b
Giselle van Dongen <giselle@restate.dev>
2025-08-20 15:10:39 +0200

    Tour of orchestration

---
+++ snippets/ts/src/tour/microservices/retries.ts
@@ -0,0 +1,34 @@
+import * as restate from "@restatedev/restate-sdk";
+import { Context } from "@restatedev/restate-sdk";
+
+type SubscriptionRequest = {
+    userId: string;
+    creditCard: string;
+    subscriptions: string[];
+}
+
+function createRecurringPayment(creditCard: string, paymentId: string) {
+    return undefined;
+}
+
+export const subscriptionService = restate.service({
+    name: "SubscriptionService",
+    handlers: {
+        add: async (ctx: Context, req: SubscriptionRequest) => {
+            const paymentId = ctx.rand.uuidv4();
+
+            // <start_retries>
+            const retryPolicy = {
+                maxRetryAttempts: 3,
+                initialRetryIntervalMillis: 1000,
+            };
+
+            const payRef = await ctx.run("pay", () =>
+                createRecurringPayment(req.creditCard, paymentId),
+                retryPolicy
+            );
+            // <end_retries>
+
+        },
+    },
+});
