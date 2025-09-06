0235e123031b561ab225f24fa6103253af43038a
Giselle van Dongen <giselle@restate.dev>
2025-09-06 13:09:13 +0200

    Agents tour

---
+++ snippets/ts/src/tour/agents/terminal_error.ts
@@ -0,0 +1,8 @@
+import * as restate from "@restatedev/restate-sdk";
+import {TerminalError} from "@restatedev/restate-sdk";
+
+async function throwError(ctx: restate.Context)  {
+    // <start_terminal_error>
+    throw new TerminalError("This tool is not allowed to run for this input.");
+    // <end_terminal_error>
+}
