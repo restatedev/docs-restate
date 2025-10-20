30aedecf8252989551a5e3980d1d432ed106f356
Giselle van Dongen <giselle@restate.dev>
2025-10-20 13:27:21 +0200

    scarf update

--- docs/scarf.js
+++ docs/scarf.js
@@ -1,1 +1,4 @@
-<img referrerpolicy="no-referrer-when-downgrade" src="https://scarf.restate.dev/a.png?x-pxid=792a64ae-afe4-487d-9eb6-5dacca7dd248" />
+const img = document.createElement('img');
+img.referrerPolicy = 'no-referrer-when-downgrade';
+img.src = 'https://scarf.restate.dev/a.png?x-pxid=792a64ae-afe4-487d-9eb6-5dacca7dd248';
+document.body.appendChild(img);
