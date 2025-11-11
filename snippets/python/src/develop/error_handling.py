b22a0a00ad52792bb2a21b75f2c65af1fdc04dc2
Francesco Guardiani <me@slinkydeveloper.com>
2025-11-11 10:06:12 +0100

    Improve documentation for Python error handling (#82)
    * Improve documentation for Python error handling
    
    * Improvements
    
    * Improvements
    
    * Update docs/develop/python/error-handling.mdx
    
    Co-authored-by: Giselle van Dongen <giselle@restate.dev>
    
    ---------
    
    Co-authored-by: Giselle van Dongen <giselle@restate.dev>

--- snippets/python/src/develop/error_handling.py
+++ snippets/python/src/develop/error_handling.py
@@ -12,3 +12,24 @@ async def my_handler(ctx: Context, arg):
 
     raise TerminalError("Something went wrong.")
     # <end_terminal>
+
+from restate.exceptions import TerminalError
+
+def undo_transaction():
+    pass
+
+@my_service.handler()
+async def my_handler_2(ctx: Context, arg):
+    # <start_run>
+    try:
+        # Await a ctx.run_typed raising an error
+        def do_transaction():
+            raise TerminalError("Can't write")
+        await ctx.run_typed("do transaction", do_transaction)
+    except TerminalError as err:
+        # Handle the terminal error raised by ctx.run_typed
+        # For example, undo previous actions...
+        await ctx.run_typed("undo transaction", undo_transaction)
+        # ...and propagate the error
+        raise err
+    # <end_run>
