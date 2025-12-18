7d2d4aa695311013e53d21d291d67aa3b185d778
Giselle van Dongen <giselle@restate.dev>
2025-12-18 12:31:12 +0100

    Fix python string interpolation and reload code snippets

--- snippets/python/src/develop/my_virtual_object.py
+++ snippets/python/src/develop/my_virtual_object.py
@@ -5,12 +5,12 @@ my_object = restate.VirtualObject("MyVirtualObject")
 
 @my_object.handler("myHandler")
 async def my_handler(ctx: restate.ObjectContext, greeting: str) -> str:
-    return f"${greeting} ${ctx.key()}!"
+    return f"{greeting} {ctx.key()}!"
 
 
 @my_object.handler(kind="shared")
 async def my_concurrent_handler(ctx: restate.ObjectSharedContext, greeting: str) -> str:
-    return f"${greeting} ${ctx.key()}!"
+    return f"{greeting} {ctx.key()}!"
 
 
 app = restate.app([my_object])
