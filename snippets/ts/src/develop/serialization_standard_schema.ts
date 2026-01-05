8bf399b78ae6674a19160f91aa7ea712dfa5fd9b
slinkydeveloper <me@slinkydeveloper.com>
2026-01-05 22:45:15 +0100

    Add support for standard schemas

---
+++ snippets/ts/src/develop/serialization_standard_schema.ts
@@ -0,0 +1,26 @@
+import { Context, WorkflowContext } from "@restatedev/restate-sdk";
+
+// <start_standard_schema>
+import * as restate from "@restatedev/restate-sdk";
+import { z } from "zod";
+
+const Greeting = z.object({
+  name: z.string(),
+});
+
+const GreetingResponse = z.object({
+  result: z.string(),
+});
+
+const greeter = restate.service({
+  name: "Greeter",
+  handlers: {
+    greet: restate.handlers.handler(
+      { input: restate.serde.schema(Greeting), output: restate.serde.schema(GreetingResponse) },
+      async (ctx: restate.Context, { name }) => {
+        return { result: `You said hi to ${name}!` };
+      }
+    ),
+  },
+});
+// <end_standard_schema>
