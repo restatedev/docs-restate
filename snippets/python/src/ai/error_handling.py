9b5eeedd3d3d26df37a42cae65291c2a004fc204
Giselle van Dongen <giselle@restate.dev>
2026-04-15 16:38:48 +0200

    update error handling pages

--- snippets/python/src/ai/error_handling.py
+++ snippets/python/src/ai/error_handling.py
@@ -1,25 +1,34 @@
 import restate
 from restate import Context, Service
-from restate.exceptions import TerminalError
-
+from restate import RunOptions
+from restate.ext.pydantic import restate_context
 
 my_service = Service("MyService")
 
 
-def fetch_data() -> str:
+def fetch_data(req: str) -> str:
     return ""
 
 
 @my_service.handler("myServiceHandler")
-async def my_service_handler(ctx: Context, greeting: str) -> str:
+async def my_service_handler(ctx: Context, req: str) -> str:
     # <start_retries>
     result = await restate_context().run_typed(
         "fetch data",
         fetch_data,
         RunOptions(max_attempts=3),
-        req=city,
+        req=req,
     )
     # <end_retries>
+
+    # <start_retries_restate>
+    result = await ctx.run_typed(
+        "fetch data",
+        fetch_data,
+        RunOptions(max_attempts=3),
+        req=req,
+    )
+    # <end_retries_restate>
 
 
     # <start_terminal>
